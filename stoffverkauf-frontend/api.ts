f05db597da5813f2d574c81e22a18d546514cf6f
digvijay315 <digvijaykumar.315@gmail.com>
2026-03-20 17:28:03 +0530

    update

--- stoffverkauf-frontend/api.ts
+++ stoffverkauf-frontend/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
+  // baseURL: "http://localhost:5000/",
+  baseURL: "https://stoffverkauf-e-commerce-1.onrender.com/",
   headers: {
     "Content-Type": "application/json",
   },
