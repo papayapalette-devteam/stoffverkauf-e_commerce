4afa8a945728cc487707344a1e79805019655f3e
digvijay315 <digvijaykumar.315@gmail.com>
2026-05-02 20:34:53 +0530

    update

--- stoffverkauf-frontend/api.ts
+++ stoffverkauf-frontend/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   // baseURL: "http://localhost:5000",
   // baseURL: "https://stoffverkauf-e-commerce-1.onrender.com"
-  baseURL: "http://13.60.202.138:5000"
+  baseURL: "https://unwrap-category-theology-put.trycloudflare.com"
 });
 
 api.interceptors.request.use((config) => {
